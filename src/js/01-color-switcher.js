648c2d087c21f6da6ef79547842d391e2c495ca0
Valerii Kalenyuk <vakabo@gmail.com>
2021-08-31 19:27:12 +0300

    adds task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,10 +1,37 @@
-refs = {
+const refs = {
   startBtn: document.querySelector('[data-start]'),
   stopBtn: document.querySelector('[data-stop]'),
 };
 
-console.log(refs.startBtn, refs.stopBtn);
+let intervalId;
+const DELAY = 1000;
 
-function colorChange() {
-  console.log('sdf');
+refs.startBtn.addEventListener('click', onBtnStartClick);
+refs.stopBtn.addEventListener('click', onBtnStopClick);
+
+function onBtnStartClick() {
+  intervalId = setIntervalImmediately(
+    () => (document.body.style.backgroundColor = getRandomHexColor()),
+    DELAY,
+  );
+  toggleBtnsActiveStatus();
+}
+
+function toggleBtnsActiveStatus() {
+  refs.startBtn.toggleAttribute('disabled');
+  refs.stopBtn.toggleAttribute('disabled');
+}
+
+function onBtnStopClick() {
+  clearInterval(intervalId);
+  toggleBtnsActiveStatus();
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function setIntervalImmediately(func, interval) {
+  func();
+  return setInterval(func, interval);
 }
