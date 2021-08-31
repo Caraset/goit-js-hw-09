62ce863218cce4650173a63a5a2d8e9fa9c1297d
Valerii Kalenyuk <vakabo@gmail.com>
2021-08-31 17:29:18 +0300

    start

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,10 @@
+refs = {
+  startBtn: document.querySelector('[data-start]'),
+  stopBtn: document.querySelector('[data-stop]'),
+};
+
+console.log(refs.startBtn, refs.stopBtn);
+
+function colorChange() {
+  console.log('sdf');
+}
